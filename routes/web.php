bfa4b95aa6fa22532538c9f609e6c2aab92149ea
Rubem Dario <rubemnascimento81@gmail.com>
2022-02-12 21:38:43 +0100

    One to One Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\{
+    User,
+    Preference
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/one-to-one', function () {
+    $user = User::first();
+
+    $data = [
+        'background_color' => '#000',
+    ];
+
+    if ($user->preference) {
+        $user->preference->update($data);
+    } else {
+        //$user->preference()->create($data); OU
+        $preference = new Preference($data);
+        $user->preference()->save($preference);
+    }
+
+
+
+    $user->refresh();
+});
 
 Route::get('/', function () {
     return view('welcome');
